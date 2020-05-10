f359ecf44d9b478b643af55d3644670e74fc64c8
Ravi Gupta <rvgupta29@gmail.com>
2020-05-10 14:35:56 +0530

    first commit

---
+++ AWS-S3-Demo/src/main/java/com/codebase/s3/CreateBucket.java
@@ -0,0 +1,36 @@
+package com.codebase.s3;
+
+import java.util.List;
+
+import com.amazonaws.auth.AWSCredentials;
+import com.amazonaws.auth.AWSStaticCredentialsProvider;
+import com.amazonaws.auth.BasicAWSCredentials;
+import com.amazonaws.services.s3.AmazonS3;
+import com.amazonaws.services.s3.AmazonS3ClientBuilder;
+import com.amazonaws.services.s3.model.AmazonS3Exception;
+import com.amazonaws.services.s3.model.Bucket;
+
+public class CreateBucket {
+
+	public static void main(String[] args) {
+		
+		String accessKey = "AKIA6JUW4KVLX2DTXLXF";
+		String secretKey = "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";
+
+
+		AWSCredentials credentials = new BasicAWSCredentials(accessKey, secretKey);
+		AmazonS3 s3 = AmazonS3ClientBuilder.standard().withCredentials(new AWSStaticCredentialsProvider(credentials)).build();
+
+		String bucket_name = "myfbww-raviguptacloud2020";
+		
+		try {
+			
+			List<Bucket> buckets = s3.listBuckets();
+			
+		}catch(AmazonS3Exception ex) {
+			ex.printStackTrace();
+		}
+	}
+	
+
+}
